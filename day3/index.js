6a0a300c2f5b339625b136b4f71f402b845e5cce
Paweł Kobryński <pawelkobrynski97@gmail.com>
2020-12-18 18:18:29 +0100

    day 3 part 2 completed! today's lesson: understand problem, lol

--- day3/index.js
+++ day3/index.js
@@ -3,20 +3,29 @@ var fs = require('fs');
 var lines = fs.readFileSync('input.txt', 'utf-8')
     .split('\n');
 
-let x = 0;
-let y = 0;
-let treeCounter = 0;
+const countTrees = (xSlip, ySlip) => {
+    let x = 0;
+    let y = 0;
+    let treeCounter = 0;
 
-let lineLength = lines[0].length;
+    let lineLength = lines[0].length;
 
-while (lines.length > y) {
-    let index = x % lineLength;
-    if (lines[y][index] === '#')
-        treeCounter++;
+    while (lines.length > y) {
+        let index = x % lineLength;
+        if (lines[y][index] === '#')
+            treeCounter++;
 
-    y += 1
-    x += 3
-
+        x += xSlip
+        y += ySlip
+    }
+    return treeCounter;
 }
 
-console.log(treeCounter);
+let c1 = countTrees(1, 1)
+let c2 = countTrees(3, 1)
+let c3 = countTrees(5, 1)
+let c4 = countTrees(7, 1)
+let c5 = countTrees(1, 2)
+
+console.log(`${c1} ${c2} ${c3}  ${c4} ${c5}`)
+console.log(c1 * c2 * c3 * c4 * c5)
