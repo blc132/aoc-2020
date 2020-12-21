cb945ac1f8e1c1a63f367a90dfa85c45b1fada42
Paweł Kobryński <pawelkobrynski97@gmail.com>
2020-12-21 10:19:38 +0100

    day 9 part 2 completed!

--- day9/Program.cs
+++ day9/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.IO;
 using System.Linq;
+using System.Numerics;
 
 using Microsoft.VisualBasic;
 
@@ -12,16 +14,64 @@ namespace day9
         static void Main(string[] args)
         {
             var lines = File.ReadAllLines("input.txt");
+            var invalidNumber = Part1(lines);
+            var encryptionWeakness = Part2(lines, invalidNumber);
+
+        }
+
+        private static BigInteger Part2(string[] lines, BigInteger invalidNumber)
+        {
+            var contiguousSet = new List<BigInteger>();
+            var contiguousSetLongest = new List<BigInteger>();
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                BigInteger sum = new BigInteger(0);
+                for (int j = i; j < lines.Length; j++)
+                {
+                    var currentNumber = BigInteger.Parse(lines[j]);
+                    sum += currentNumber;
+
+                    contiguousSet.Add(currentNumber);
+
+
+                    if (sum == invalidNumber)
+                    {
+                        if (contiguousSetLongest.Count < contiguousSet.Count)
+                            contiguousSetLongest = contiguousSet.ConvertAll(x => x);
+
+                        contiguousSet.Clear();
+                        break;
+                    }
+
+                    if (sum > invalidNumber)
+                    {
+                        contiguousSet.Clear();
+                        break;
+                    }
+                }
+            }
+
+            contiguousSetLongest.Sort();
+
+            return contiguousSetLongest.First() + contiguousSetLongest.Last();
+        }
+
+
+        public static BigInteger Part1(string[] lines)
+        {
 
             int preambleLength = 25;
-            int nextNumber;
-            List<int> preamble;
+            BigInteger nextNumber = 0;
+            List<BigInteger> preamble;
 
             for (int i = 4; i < lines.Length; i++)
             {
+                if (i + preambleLength >= lines.Length)
+                    break;
 
-                preamble = lines.Skip(i).Take(preambleLength).Select(int.Parse).ToList();
-                nextNumber = int.Parse(lines.Skip(i + preambleLength).First());
+                preamble = lines.Skip(i).Take(preambleLength).Select(BigInteger.Parse).ToList();
+                nextNumber = BigInteger.Parse(lines.Skip(i + preambleLength).First());
 
 
                 var correct = CheckNumber(preamble, nextNumber);
@@ -29,10 +79,11 @@ namespace day9
                 if (!correct)
                     break;
             }
+
+            return nextNumber;
         }
 
-
-        public static bool CheckNumber(List<int> preamble, int number)
+        public static bool CheckNumber(List<BigInteger> preamble, BigInteger number)
         {
             for (int i = 0; i < preamble.Count; i++)
             {
